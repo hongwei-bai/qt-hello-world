d0b8c7e0cb3b5b40935973b12e58dc7ad6754ee9
Hongwei <bhw8412@hotmail.com>
2021-06-15 10:59:50 +1000

    lesson 11 displaying image using label in qt

--- LoginApp/mainwindow.cpp
+++ LoginApp/mainwindow.cpp
@@ -1,12 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QMessageBox>
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap pix("D:/code/qt/qt-hello-world/images/ad.png");
+//    ui->label_pic->setPixmap(pix.scaled(100,100,Qt::KeepAspectRatio));
+
+    int w = ui->label_pic->width();
+    int h = ui->label_pic->height();
+    ui->label_pic->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatio));
 }
 
 MainWindow::~MainWindow()
